33758c2c324fa26841379378b41eb0fb07a6b1b9
Torreya01 <boyuann2001@gmail.com>
2024-04-20 20:01:25 +0800

    Update the data-clustering file

--- source/data-clustering.R
+++ source/data-clustering.R
@@ -6,11 +6,11 @@ data_reduced <- read.csv("data/derived/location_reduced.csv")
 # Determine the number of clusters
 fviz_nbclust(data_reduced[,c("lat","long")], kmeans, method = "wss")
 
-# Perform kmeans clustering
-m_km <- kmeans(data_reduced[,c("lat","long")], 10)
+# Perform kmeans to cluster the data into 10 clusters
+m_km = kmeans(data_reduced[,c("lat","long")], 10)
 
 # Add the cluster assignment as a factor to the data frame
-data_reduced$cluster <- factor(m_km$cluster)
+data_reduced$cluster = factor(m_km$cluster)
 
 ## Save the data
 write.csv(data_reduced, "Data/Derived/location_clustered.csv", row.names = FALSE)
