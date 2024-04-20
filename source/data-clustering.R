16c41ece8719ea291fe9a0167170ca5f2f791be5
Torreya01 <boyuann2001@gmail.com>
2024-04-20 19:55:52 +0800

    Upload the data-clustering file

---
+++ source/data-clustering.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+
+# Load the data
+data_reduced <- read.csv("data/derived/location_reduced.csv")
+
+# Determine the number of clusters
+fviz_nbclust(data_reduced[,c("lat","long")], kmeans, method = "wss")
+
+# Perform kmeans clustering
+m_km <- kmeans(data_reduced[,c("lat","long")], 10)
+
+# Add the cluster assignment as a factor to the data frame
+data_reduced$cluster <- factor(m_km$cluster)
+
+## Save the data
+write.csv(data_reduced, "Data/Derived/location_clustered.csv", row.names = FALSE)
