ef9bf60f0178947248f2602b20fd865a72929947
Torreya01 <boyuann2001@gmail.com>
2024-05-06 20:49:39 +0100

    Update the data-cleaning file

--- source/data-cleaning.R
+++ source/data-cleaning.R
@@ -5,12 +5,10 @@ library(tidyverse)
 # Import data set from the raw data folder
 data = read.csv("data/raw/kc_house_data.csv", header = TRUE)
 
-# The codes below are used for extracting data for RShiny app
-
-## Extract data that only contains the house prices and location information
+# Extract data that only contains the house prices and location information
 data_loc = data %>% select("price", "lat", "long")
 
-## Omit the NAs from the data we derived from the raw dataset
+# Omit the NAs from the data we derived from the raw dataset
 data_loc = na.omit(data_loc)
 
 ## Save the data
