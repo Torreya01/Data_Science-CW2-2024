9eaef2cd3d3a6f7f4e165eacb480196555c390c7
Torreya01 <boyuann2001@gmail.com>
2024-05-08 22:06:21 +0100

    Update the fifth version of the application

--- app.R
+++ app.R
@@ -1,33 +1,38 @@
-# Load necessary libraries
 # Load libraries
 library(shiny)
 library(leaflet)
 library(dplyr)
 library(DT)
 library(bslib)
-library(ggplot2)
+
+# Source the helper functions
+source("source/helper-functions/density-plot.R")
+source("source/helper-functions/box-plot.R")
+source("source/helper-functions/colour-patterns.R")
+source("source/helper-functions/map.R")
 
 # Define UI
 ui = navbarPage("House Prices in King County",
                 theme = bs_theme(version = 5, bootswatch = "minty"),
                 tabPanel("Map",
-                          sidebarLayout(
-                            sidebarPanel(
-                              sliderInput("priceRange",
-                                          "Price Range:",
-                                          min = 0,
-                                          max = 1,
-                                          value = c(0, 1),
-                                          step = 50000,
-                                          pre = "$"),
-                              div(style = "display: 2:5; flex-wrap: wrap;",
-                                  checkboxGroupInput("selectedClusters",
-                                                     "Select Clusters:",
-                                                     choices = as.character(1:10),
-                                                     selected = 1:10,
-                                                     inline = TRUE)
-                              )
-                            ),
+                         sidebarLayout(
+                           sidebarPanel(
+                             sliderInput("priceRange",
+                                         "Price Range:",
+                                         min = 0,
+                                         max = 1,
+                                         value = c(0, 1),
+                                         step = 50000,
+                                         pre = "$"),
+                             selectInput("yearSelect", "Select Year:",
+                                         choices = c("2014", "2015", "All")),
+                             checkboxGroupInput("selectedClusters",
+                                                "Select Clusters:",
+                                                choices = as.character(1:10),
+                                                selected = 1:10,
+                                                inline = TRUE),
+                             plotOutput("mapdensity")
+                           ),
                             mainPanel(
                               h3("Houses on the map of King County"),
                               
@@ -44,31 +49,31 @@ ui = navbarPage("House Prices in King County",
                               
                               p("Due to the large data set, the application may 
                               be slow to run, please be patient when the application
-                              is running."),
+                              is running."), 
                               
-                              h3("Data Selection"),
+                              p("The plot is a scatterplot visualizing house data from King County, plotted by latitude and longitude, 
+                          categorized into 10 different clusters via k-means. Each cluster 
+                          is represented by a unique color. K-means is a popular clustering algorithm 
+                          used in data analysis and machine learning to partition a set of observations 
+                          into a specified number of clusters, where each observation belongs 
+                          to the cluster with the nearest mean."),
                               
-                              p("The minimum of the data set is 75000 and the maximum
-                              is 7700000. Since the full data set has 21613 observations,
-                              which is too big for the application to operate. 
-                              Therefore, I reduced the data set into 1997 observations."),
+                              p("Some clusters like 3, 5, 7 and 9 are more densely packed, indicating
+                          areas with high housing density or popular residential areas. Other clusters
+                          are more spread out, which reflects the population density 
+                          or the type of housing available in those areas are not that many. Especially for 
+                          cluster 1 and 4, they have less houses in these regions and might represent
+                          suburban or rural areas for these clusters."),
                               
-                              p("I splitted the data into 10 clusters: I assign 2000 
-                              observtions based on the percentages. However, due to the 
-                              decimal points when calculating the percentages for each 
-                              clusters, the total observations would be 1997. 
-                              For simplicity, I will look into the reduced data set 
-                              instead of the full data set."),
+                              p("Cluster 10 appears quite isolated and far from 
+                          other clusters, located at higher latitudes and longitudes. This might indicate
+                          a unique geographic or socioeconomic area. We can see on the map that it could be
+                          a distinct community separated from central urban areas."),
                             )
                           )),
                 
                 tabPanel("Density plot",
                          h3("Density plot with a density curve"),
-                         
-                         div(style = "text-align: center;",  # Center the image and caption
-                             img(src = "figures/Histogram.png", height = "600px", width = "800px"),
-                             p("Histogram of the house prices with density in King County, USA")
-                         ),
                          
                          p("The x-axis represents the price of houses in King County, 
                           the y-axis represents the density of the distribution, showing
@@ -112,45 +117,16 @@ ui = navbarPage("House Prices in King County",
                          
                 ),
                           
-                 tabPanel("Non-Parametric Analysis",
-                          p("Below are some non-parametric statistical analysis about the 
-                            house data. We will first look at how 10 different clusters are 
-                            located in the map, and then discuss the density of the whole
-                            data set. Finally, we will look at a box plot for each clusters 
-                            so that we can find out which region has the most expensive houses
-                            and look for the reasons."),
-                          
-                          h3("10 different house clusters"),
-                          
-                          div(style = "text-align: center;",  # Center the image and caption
-                              img(src = "figures/Cluster.png", height = "600px", width = "800px"),
-                              p("10 different clusters shown on the King County map")),
-                          
-                          p("The plot is a scatterplot visualizing house data from King County, plotted by latitude and longitude, 
-                          categorized into 10 different clusters via k-means. Each cluster 
-                          is represented by a unique color. K-means is a popular clustering algorithm 
-                          used in data analysis and machine learning to partition a set of observations 
-                          into a specified number of clusters, where each observation belongs 
-                          to the cluster with the nearest mean."),
-    
-                          p("Some clusters like 3, 5, 7 and 9 are more densely packed, indicating
-                          areas with high housing density or popular residential areas. Other clusters
-                          are more spread out, which reflects the population density 
-                          or the type of housing available in those areas are not that many. Especially for 
-                          cluster 1 and 4, they have less houses in these regions and might represent
-                          suburban or rural areas for these clusters."),
-                          
-                          p("Cluster 10 appears quite isolated and far from 
-                          other clusters, located at higher latitudes and longitudes. This might indicate
-                          a unique geographic or socioeconomic area. We can see on the map that it could be
-                          a distinct community separated from central urban areas."),
-                          
-                          h3("The boxplot for 10 different clusters"),
-                          
-                          div(style = "text-align: center;",  # Center the image and caption
-                              img(src = "figures/Box.png", height = "600px", width = "800px"),
-                              p("Box plot of the clusters showing differnt variability")
-                           ),
+                tabPanel("Box plot",
+                         fluidRow(
+                           column(12, 
+                                  selectInput("yearInput", 
+                                              "Select Year:", 
+                                              choices = c(2014, 2015),
+                                              selected = 2014))),
+                         fluidRow(
+                           column(12,
+                                  plotOutput("boxplot", width = "100%"))),
                           
                           p("Through the box plot of the data, I can visualize the distribution
                           of house prices across 10 different clusters."),
@@ -228,140 +204,79 @@ ui = navbarPage("House Prices in King County",
                           p("Details about the transportation..."))
 )
 
-
 # Define server
 server = function(input, output, session) {
   
   # Read data from a CSV file
-  house = read.csv("data/derived/location_reduced.csv", stringsAsFactors = FALSE)
+  house = read.csv("data/derived/location_clustered.csv", stringsAsFactors = FALSE)
   
-  # Observe changes in data and update the slider accordingly
+  # Map clusters to colours
+  colour_map = setNames(colour_choice, unique(house$cluster))
+  
   observe({
-    updateSliderInput(session, "priceRange",
-                      min = min(house$price, na.rm = TRUE),
-                      max = max(house$price, na.rm = TRUE),
-                      value = c(min(house$price, na.rm = TRUE), max(house$price, na.rm = TRUE)))
-    
-    updateCheckboxGroupInput(session, "selectedClusters",
-                             choices = levels(factor(house$cluster)),
-                             selected = levels(factor(house$cluster)))
+    if (input$yearSelect == "All") {
+      updateSliderInput(session, "priceRange",
+                        min = min(house$price, na.rm = TRUE),
+                        max = max(house$price, na.rm = TRUE),
+                        value = c(min, max))
+    } 
+    else {
+      updateSliderInput(session, "priceRange",
+                        min = min(house$price[house$date == input$yearSelect], na.rm = TRUE),
+                        max = max(house$price[house$date == input$yearSelect], na.rm = TRUE),
+                        value = c(min, max))
+    }
   })
-
+  
+  
   # Update reactive expression for filtered data
   filteredData = reactive({
-    house %>%
+    if (input$yearSelect == "All") {
+      # Filter data without considering the year
+      filtered = house %>%
+        filter(price >= input$priceRange[1], price <= input$priceRange[2],
+               as.character(cluster) %in% input$selectedClusters)
+    } 
+    else {
+    filtered = house %>%
+      filter(date %in% input$yearSelect) %>%
       filter(price >= input$priceRange[1], price <= input$priceRange[2]) %>%
-      filter(as.character(cluster) %in% input$selectedClusters)  # Filter for selected clusters
+      filter(as.character(cluster) %in% input$selectedClusters)
+    }
+    # Map color based on cluster
+    filtered$color = colour_map[as.character(filtered$cluster)]
+    filtered
   })
   
   # Render the map output
   output$map = renderLeaflet({
-    filtered_data = filteredData()  # Get filtered data based on slider input
-    
-    # Define the bounds for King County to restrict the map view
-    bounds = list(
-      southWest = list(lat = 47.2, lng = -122.5),
-      northEast = list(lat = 47.9, lng = -121.5)
-    )
-    
-    # Create the map with initial view settings
-    map = leaflet(filtered_data) %>%
-      addTiles() %>%
-      setView(lng = -121.9836, lat = 47.5480, zoom = 9) %>%
-      setMaxBounds(lng1 = bounds$southWest$lng, lat1 = bounds$southWest$lat,
-                   lng2 = bounds$northEast$lng, lat2 = bounds$northEast$lat)
-    
-    # Add markers to the map if data is available
-    if(nrow(filtered_data) > 0) {  
-      map = map %>% addMarkers(~long, ~lat, popup = ~paste("Price: $", price))
-    }
-    
-    map  # Return the map object
+    mapproduce(filteredData())
+  })
+  
+  output$mapdensity = renderPlot({
+    densityplot(filteredData())
   })
   
   output$densityPlot1 = renderPlot({
-    
-    # Filter data based on the selected cluster
     data = house
     if (input$clusterSelection1 != "All") {
       data = data[data$cluster == as.numeric(input$clusterSelection1), ]
     }
-    
-    # Calculate the density
-    density_data = density(data$price, adjust = 4)
-    max_density_x = density_data$x[which.max(density_data$y)]
-    max_density_y = max(density_data$y)
-    # Create a data frame for the vertical line
-    line_data = data.frame(x = c(max_density_x, max_density_x), 
-                           y = c(0, max_density_y))
-    
-    # Generate the density plot
-    ggplot(data, aes(x = price)) +
-      # Normalise the histogram
-      geom_histogram(aes(y = after_stat(density)),
-                     # Adjust binwidth to the scale of the data
-                     binwidth = 50000,
-                     color = "black", fill = "lightblue", alpha = 0.7) +
-      geom_density(aes(y = after_stat(density)),  
-               # Adjust the color of the density curve
-               color = "red", 
-               # Adjust the size of the density curve
-               linewidth = 1,
-               # Adjust the smoothing parameter for density
-               adjust = 4) + 
-      labs(title = paste("Density Plot for Cluster", input$clusterSelection1),
-           x = "Price",
-           y = "Density") +
-      xlim(0, 8000000) +
-      # Add vertical line at max density
-      geom_segment(aes(x = x, y = 0, xend = x, yend = y), 
-                   data = line_data,
-                   color = "blue", linewidth = 1, linetype = "dashed") +
-      # Add label max density
-      geom_text(aes(x = max_density_x, y = 0, label = round(max_density_x, 2)), 
-                color = "blue", size = 3, vjust = 1.5)
+    densityplot(data, input$clusterSelection1)
   })
   
   output$densityPlot2 = renderPlot({
-    # Filter data based on the selected cluster
     data = house
     if (input$clusterSelection2 != "All") {
       data = data[data$cluster == as.numeric(input$clusterSelection2), ]
     }
-    
-    # Calculate the density
-    density_data = density(data$price, adjust = 4)
-    max_density_x = density_data$x[which.max(density_data$y)]
-    max_density_y = max(density_data$y)
-    # Create a data frame for the vertical line
-    line_data = data.frame(x = c(max_density_x, max_density_x), 
-                           y = c(0, max_density_y))
-    
-    # Generate the density plot
-    ggplot(data, aes(x = price)) +
-      # Normalise the histogram
-      geom_histogram(aes(y = after_stat(density)),
-                     # Adjust binwidth to the scale of the data
-                     binwidth = 50000,
-                     color = "black", fill = "lightblue", alpha = 0.7) +
-      geom_density(aes(y = after_stat(density)),  
-                   # Adjust the color of the density curve
-                   color = "red", 
-                   # Adjust the size of the density curve
-                   linewidth = 1,
-                   # Adjust the smoothing parameter for density
-                   adjust = 4) + 
-      labs(title = paste("Density Plot for Cluster", input$clusterSelection2),
-           x = "Price",
-           y = "Density") +
-      xlim(0, 8000000) +
-      # Add vertical line at max density
-      geom_segment(aes(x = x, y = 0, xend = x, yend = y), 
-                   data = line_data,
-                   color = "blue", linewidth = 1, linetype = "dashed") +
-      # Add label max density
-      geom_text(aes(x = max_density_x, y = 0, label = round(max_density_x, 2)), 
-                color = "blue", size = 3, vjust = 1.5)
+    densityplot(data, input$clusterSelection2)
+  })
+  
+  output$boxplot = renderPlot({
+    data = house
+    data = data[data$date == as.numeric(input$yearInput), ]
+    boxplot(data, input$yearInput)
   })
   
 }
