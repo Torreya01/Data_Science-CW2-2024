24515d22523410432ec2a63dcd212b2b8c94ac8c
Torreya01 <boyuann2001@gmail.com>
2024-05-08 21:21:30 +0100

    Update the data-processing file

--- source/data-cleaning.R
+++ source/data-cleaning.R
@@ -6,7 +6,11 @@ library(tidyverse)
 data = read.csv("data/raw/kc_house_data.csv", header = TRUE)
 
 # Extract data that only contains the house prices and location information
-data_loc = data %>% select("price", "lat", "long")
+data_loc = data %>% select("price", "date", "lat", "long")
+
+# Extract the year and convert it to integer
+data_loc = data_loc %>%
+  mutate(date = as.integer(substr(date, 1, 4)))
 
 # Omit the NAs from the data we derived from the raw dataset
 data_loc = na.omit(data_loc)
--- source/data-clustering.R
+++ source/data-clustering.R
@@ -1,16 +1,17 @@
 library(ggplot2)
 
 # Load the data
-data_reduced <- read.csv("data/derived/location.csv")
+data <- read.csv("data/derived/location.csv")
 
 # Determine the number of clusters
-fviz_nbclust(data_reduced[,c("lat","long")], kmeans, method = "wss")
+fviz_nbclust(dat[,c("lat","long")], kmeans, method = "wss")
 
 # Perform kmeans to cluster the data into 10 clusters
-m_km = kmeans(data_reduced[,c("lat","long")], 10)
+m_km = kmeans(data[,c("lat","long")], 10)
 
 # Add the cluster assignment as a factor to the data frame
-data_reduced$cluster = factor(m_km$cluster)
+data$cluster = factor(m_km$cluster)
 
 ## Save the data
-write.csv(data_reduced, "Data/Derived/location_clustered.csv", row.names = FALSE)
+write.csv(data, "Data/Derived/location_clustered.csv", row.names = FALSE)
+
