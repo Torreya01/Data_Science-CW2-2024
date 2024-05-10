bb8b67438ae3537e310b8b1af6fa955f42e4bbe6
Torreya01 <boyuann2001@gmail.com>
2024-05-10 22:15:06 +0100

    Upload the data filter file

---
+++ source/helper-functions/data-filter.R
@@ -0,0 +1,20 @@
+# Load library
+library(dplyr)
+
+filterData = function(house, priceRange, selectedClusters, yearSelect, colour_map) {
+  if (yearSelect == "All") {
+    # Filter data without considering the year
+    filtered = house %>%
+      dplyr::filter(price >= priceRange[1], price <= priceRange[2],
+                    as.character(cluster) %in% selectedClusters)
+  } else {
+    # Filter data considering the specific year
+    filtered = house %>%
+      filter(date %in% yearSelect) %>%
+      filter(price >= priceRange[1], price <= priceRange[2]) %>%
+      filter(as.character(cluster) %in% selectedClusters)
+  }
+  # Map color based on cluster
+  filtered$color = colour_map[as.character(filtered$cluster)]
+  return(filtered)
+}
